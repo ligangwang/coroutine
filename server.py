54cb0c909e0908ade28b2dc86b1cbaec5a929f29
knoics <ligangwangs@gmail.com>
2023-10-02 00:05:28 -0400

    add more files

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 import asyncio
 from websockets.server import serve
 import websockets
+from concurrent.futures import ProcessPoolExecutor
+from service import on_msg_recv
 
 connected = set()
-
+executor = ProcessPoolExecutor()
+loop = asyncio.get_event_loop()
 
 async def connect(ws):
     global connected
@@ -26,28 +29,39 @@ async def send_to_clients(msg):
     print(f'broadcasting to {len(connected)} clients: {msg}')
     # websockets.broadcast(connected, f'relayed: {msg}')
 
+async def receive_from_market(msg):
+    global executor
+    await loop.run_in_executor(executor, on_msg_recv, msg)
+
 async def server_main():
     async with serve(connect, "localhost", 8764):
         try:
-
             await asyncio.Future()
         except asyncio.CancelledError:
             print ('canceled')
             return
 
-async def client_main():
-    async with websockets.connect('ws://localhost:8765') as ws:
-        async for msg in ws:
-            await send_to_clients(msg)
+async def market_main():
+    async for ws in websockets.connect('ws://localhost:8765'):
+        try:
+            async for msg in ws:
+                await receive_from_market(msg)
+        except ConnectionRefusedError:# (ConnectionAbortedError, ConnectionRefusedError,ConnectionResetError, ConnectionError):
+            continue
+        except:
+            print('catch call')
 
 async def main():
-    await asyncio.gather(
-        server_main(),
-        client_main()
-    )
+    try:
+        await asyncio.gather(
+            server_main(),
+            market_main()
+        )
+    except ConnectionRefusedError:
+        print('market connection failed')
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, ConnectionRefusedError):
         print('bye!')
---
+++ service.py
@@ -0,0 +1,4 @@
+from datastore import save_in_messages
+
+def on_msg_recv(msg):
+    pass
